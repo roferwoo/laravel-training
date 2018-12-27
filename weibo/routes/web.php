96a03ced5fbc6e95b47068727e045181b1b58d86
roferwoo <roferwoo@gmail.com>
2018-12-27 07:53:35 +0000

    Finish user signup

--- weibo/routes/web.php
+++ weibo/routes/web.php
@@ -22,3 +22,38 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 // 用户注册
 Route::get('signup', 'UsersController@create')->name('signup');
+// 用户资源路由
+Route::resource('users', 'UsersController');
+// 等同于
+// Route::get('/users', 'UsersController@index')->name('users.index');// 显示所有用户列表的页面
+// Route::get('/users/create', 'UsersController@create')->name('users.create');// 创建用户的页面
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');// 显示用户个人信息的页面
+// Route::post('/users', 'UsersController@store')->name('users.store');// 创建用户
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');// 编辑用户个人资料的页面
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');// 更新用户
+// Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');// 删除用户
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
