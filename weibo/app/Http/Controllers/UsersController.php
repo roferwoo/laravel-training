15c6db9bd67f08ac43296e4751022f9d153f7655
roferwoo <roferwoo@gmail.com>
2019-01-02 07:43:03 +0000

    Add password resets & email configuration

--- weibo/app/Http/Controllers/UsersController.php
+++ weibo/app/Http/Controllers/UsersController.php
@@ -125,6 +125,7 @@ class UsersController extends Controller
 
     protected function sendEmailConfirmationTo($user)
     {
+        /*
         $view = 'emails.confirm';
         $data = compact('user');
         $from = 'roferwoo@163.com';
@@ -134,6 +135,16 @@ class UsersController extends Controller
 
         Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
             $message->from($from, $name)->to($to)->subject($subject);
+        });
+        */
+
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $to = $user->email;
+        $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 }
--- weibo/routes/web.php
+++ weibo/routes/web.php
@@ -38,7 +38,14 @@ Route::get('login', 'SessionsController@create')->name('login');// 显示登录�
 Route::post('login', 'SessionsController@store')->name('login');// 创建新会话（登录）
 Route::delete('logout', 'SessionsController@destroy')->name('logout');// 销毁会话（退出登录）
 
-Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');// 激活
+// 邮箱激活
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+// 重置密码
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');// 显示重置密码的邮箱发送页面
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');// 邮箱发送重设链接
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');// 密码更新页面
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');// 执行密码更新操作
 
 
 
