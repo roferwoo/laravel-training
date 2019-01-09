55e2181b02bfae153d0401921501e1406d3f6f84
roferwoo <roferwoo@gmail.com>
2019-01-09 08:34:44 +0000

    Generated topics

--- bbs/routes/web.php
+++ bbs/routes/web.php
@@ -41,3 +41,5 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 // Route::get('/users/{user}', 'UsersController@show')->name('users.show');// 显示用户个人信息页面
 // Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');// 显示编辑个人资料页面
 // Route::patch('/users/{user}', 'UsersController@update')->name('users.update');// 处理edit页面提交的修改
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
