2e61b568ec17bc1544866e3a054e18b7e766df43
roferwoo <roferwoo@gmail.com>
2019-01-10 14:28:17 +0000

    User can upload topic image

--- bbs/app/Http/Controllers/TopicsController.php
+++ bbs/app/Http/Controllers/TopicsController.php
@@ -6,12 +6,36 @@ use App\Models\Category;
 use App\Models\Topic;
 use App\Http\Requests\TopicRequest;
 use Auth;
+use App\Handlers\ImageUploadHandler;
+use App\Http\Requests\Request;
 
 class TopicsController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据，默认是失败的
+        $data = [
+            'success'   => false,
+            'msg'       => '上传失败!',
+            'file_path' => ''
+        ];
+        // 判断是否有上传文件，并赋值给 $file
+        if ($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
+            // 图片保存成功的话
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg']       = "上传成功!";
+                $data['success']   = true;
+            }
+        }
+        return $data;
     }
 
     public function index(TopicRequest $request, Topic $topic)
--- bbs/routes/web.php
+++ bbs/routes/web.php
@@ -47,3 +47,7 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+
+// 富文本编辑器Simditor 上传图片
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
