9b67a913cb30dce42302f9e215061336f7147b37
roferwoo <roferwoo@gmail.com>
2019-01-14 02:29:38 +0000

    Add queue for translate slug

--- bbs/app/Handlers/SlugTranslateHandler.php
+++ bbs/app/Handlers/SlugTranslateHandler.php
@@ -7,39 +7,36 @@ use Overtrue\Pinyin\Pinyin;
 
 class SlugTranslateHandler
 {
+    //初始化配置信息
+    private $api;
+    private $appid;
+    private $key;
+
+    //配置翻译方式
+    private $from;
+    private $to;
+
+    public function __construct($from = 'zh', $to = 'en')
+    {
+        // 初始化配置信息
+        $this->appid = config('services.baidu_translate.appid');
+        $this->key = config('services.baidu_translate.key');
+        $this->api = 'http://api.fanyi.baidu.com/api/trans/vip/translate?';
+        $this->from = $from;
+        $this->to = $to;
+    }
+
     public function translate($text)
     {
-        // 实例化 HTTP 客户端
-        $http = new Client;
-
-        // 初始化配置信息
-        $api = 'http://api.fanyi.baidu.com/api/trans/vip/translate?';
-        $appid = config('services.baidu_translate.appid');
-        $key = config('services.baidu_translate.key');
-        $salt = time();
-
         // 如果没有配置百度翻译，自动使用兼容的拼音方案
-        if (empty($appid) || empty($key)) {
+        if (empty($this->appid) || empty($this->key)) {
             return $this->pinyin($text);
         }
 
-        // 根据文档，生成 sign
-        // http://api.fanyi.baidu.com/api/trans/product/apidoc
-        // appid+q+salt+密钥 的MD5值
-        $sign = md5($appid. $text . $salt . $key);
-
-        // 构建请求参数
-        $query = http_build_query([
-            "q"     =>  $text,
-            "from"  => "zh",
-            "to"    => "en",
-            "appid" => $appid,
-            "salt"  => $salt,
-            "sign"  => $sign,
-        ]);
-
+        // 实例化 HTTP 客户端
+        $http = new Client;
         // 发送 HTTP Get 请求
-        $response = $http->get($api.$query);
+        $response = $http->get($this->query($text));
 
         $result = json_decode($response->getBody(), true);
 
@@ -71,5 +68,27 @@ class SlugTranslateHandler
     public function pinyin($text)
     {
         return str_slug(app(Pinyin::class)->permalink($text));
+    }
+
+    // 创建请求语句
+    private function query($text)
+    {
+        // 根据百度翻译API文档，生成 sign
+        // http://api.fanyi.baidu.com/api/trans/product/apidoc
+        // appid+q+salt+密钥 的MD5值
+        $salt = time();
+        $sign = md5($this->appid . $text . $salt . $this->key);
+
+        //构建请求参数
+        $query = http_build_query([
+            'q' => $text,
+            'from' => $this->from,
+            'to' => $this->to,
+            'appid' => $this->appid,
+            'salt' => $salt,
+            'sign' => $sign,
+        ]);
+
+        return $this->api . $query;
     }
 }
