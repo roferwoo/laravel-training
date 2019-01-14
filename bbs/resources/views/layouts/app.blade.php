496a5cb30862f59cb20b6388921f1cf7e445c0d7
roferwoo <roferwoo@gmail.com>
2019-01-14 15:56:07 +0000

    Admin can manage site settings

--- bbs/resources/views/layouts/app.blade.php
+++ bbs/resources/views/layouts/app.blade.php
@@ -8,8 +8,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'BBS') - Laravel 进阶教程</title>
-    <meta name="description" content="@yield('description', 'BBS Laravel爱好者社区')" />
+    <title>@yield('title', 'BBS') - {{ setting('site_name', 'Laravel 进阶教程') }}</title>
+    <meta name="description" content="@yield('description', setting('seo_description', 'BBS Laravel爱好者社区。'))" />
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'Laravel,BBS,社区,论坛,开发者论坛'))" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
