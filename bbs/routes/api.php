ee098330a89384fc70f3c78e583e6227c3900e5f
roferwoo <roferwoo@gmail.com>
2019-01-17 03:50:21 +0000

    add easysms

--- bbs/routes/api.php
+++ bbs/routes/api.php
@@ -19,12 +19,12 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
+// 测试 v1 v2
 $api->version('v1', function($api) {
     $api->get('version', function() {
         return response('this is version v1');
     });
 });
-
 $api->version('v2', function($api) {
     $api->get('version', function() {
         return response('this is version v2');
