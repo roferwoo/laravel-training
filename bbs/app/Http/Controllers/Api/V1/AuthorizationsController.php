b9cd6d4fcb52853f35581b0bab32be69a09829ea
roferwoo <roferwoo@gmail.com>
2019-01-20 02:58:22 +0000

    generate-token command

--- bbs/app/Http/Controllers/Api/V1/AuthorizationsController.php
+++ bbs/app/Http/Controllers/Api/V1/AuthorizationsController.php
@@ -4,9 +4,46 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Requests\Api\V1\SocialAuthorizationRequest;
 use App\Models\User;
+use App\Http\Requests\Api\V1\AuthorizationRequest;
+use Auth;
 
 class AuthorizationsController extends ApiController
 {
+    public function store(AuthorizationRequest $request)
+    {
+        $username = $request->username;
+
+        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+
+        $credentials['password'] = $request->password;
+
+        if (!$token = Auth::guard('api')->attempt($credentials)) {
+            return $this->response->errorUnauthorized('用户名或密码错误');
+        }
+
+        // return $this->response->array([
+        //     'access_token' => $token,
+        //     'token_type' => 'Bearer',
+        //     'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        // ])->setStatusCode(201);
+
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    public function update()
+    {
+        $token = Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        Auth::guard('api')->logout();
+        return $this->response->noContent();
+    }
+
     public function socialStore($type, SocialAuthorizationRequest $request)
     {
         if (!in_array($type, ['weixin'])) {
@@ -55,6 +92,18 @@ class AuthorizationsController extends ApiController
                 break;
         }
 
-        return $this->response->array(['token' => $user->id]);
+        // return $this->response->array(['token' => $user->id]);
+        $token = Auth::guard('api')->fromUser($user);
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            // jwt-auth 中过期时间单位是分，接口转换成秒 所以 *60
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
+        ]);
     }
 }
---
+++ bbs/app/Http/Requests/Api/V1/AuthorizationRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+
+class AuthorizationRequest extends ApiRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username' => 'required|string',
+            'password' => 'required|string|min:6',
+        ];
+    }
+}
