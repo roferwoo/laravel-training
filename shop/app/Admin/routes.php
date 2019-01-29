3212a3fd84b9d955b78f0235e06de2de2f316728
roferwoo <roferwoo@gmail.com>
2019-01-29 07:17:21 +0000

    管理后台 - 新增、编辑商品

--- shop/app/Admin/routes.php
+++ shop/app/Admin/routes.php
@@ -13,7 +13,12 @@ Route::group([
     $router->get('/', 'HomeController@index');
     // 用户管理
     $router->get('users', 'UsersController@index');
+
     // 商品管理
-    $router->get('products', 'ProductsController@index');
+    $router->get('products', 'ProductsController@index');// 列表
+    $router->get('products/create', 'ProductsController@create');// 添加页面
+    $router->post('products', 'ProductsController@store');// 添加保存
+    $router->get('products/{id}/edit', 'ProductsController@edit');// 编辑页面
+    $router->put('products/{id}', 'ProductsController@update');// 编辑保存
 
 });
