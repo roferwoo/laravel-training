e56a45fea78793bcbe5ea07a8485c66438f89f67
roferwoo <roferwoo@gmail.com>
2019-01-31 08:27:35 +0000

    引入支付库

--- shop/app/Http/Controllers/OrdersController.php
+++ shop/app/Http/Controllers/OrdersController.php
@@ -11,6 +11,7 @@ use App\Models\Order;
 use Carbon\Carbon;
 use App\Exceptions\InvalidRequestException;
 use App\Jobs\CloseOrder;
+use App\Services\OrderService;
 
 class OrdersController extends Controller
 {
