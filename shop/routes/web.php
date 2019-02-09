44780db9755c168bbd95401c3e939760f3a8a3cb
roferwoo <roferwoo@gmail.com>
2019-02-09 01:00:17 +0000

    用户界面 - 检查优惠券

--- shop/routes/web.php
+++ shop/routes/web.php
@@ -101,6 +101,9 @@ Route::group([
         // 申请退款
         Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
 
+        // 检查优惠券
+        Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
+
 
         // 测试中间件 email_verified
         Route::get('/test', function() {
